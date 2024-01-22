df019c5c62cee44cf3bf4c7f8337f92952e3c27a
tejasvi raj <tejasviraj8686@gmail.com>
2024-01-22 23:11:33 +0530

    first init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import Formdata from "./components/formdata/Formdata";
+
+const App = () => {
+  return (
+    <div>
+      <Formdata />
+    </div>
+  );
+};
+
+export default App;
